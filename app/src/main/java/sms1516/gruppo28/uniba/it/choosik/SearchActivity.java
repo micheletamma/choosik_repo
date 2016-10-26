8c7f34912f532c79a0387b1d16488d79424f624a
micheletamma <michele.tamma@gmail.com>
2016-10-26 19:32:26 +0200

    implementata SearchActivity

--- app/src/main/java/sms1516/gruppo28/uniba/it/choosik/SearchActivity.java
+++ app/src/main/java/sms1516/gruppo28/uniba/it/choosik/SearchActivity.java
@@ -1,36 +1,80 @@
 package sms1516.gruppo28.uniba.it.choosik;
 
-import android.content.Intent;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
-public class SearchActivity extends MainActivity
+import java.util.Calendar;
+
+/**
+ * La classe Search Activity e' estesa a MainActivity in modo tale da implementare
+ * il nav_menu
+ */
+public class SearchActivity extends AppCompatActivity
 {
+    /**
+     * Nel metodo onCreate carichiamo lo stesso menu della Main Activity
+     * Si implementa un Date Picker per gestire le date in input
+     */
+
+    TextView mDateDisplay;
+    Button mPickDate;
+    int mYear;
+    int mMonth;
+    int mDay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_search);
-        FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame); //Remember this is the FrameLayout area within your activity_main.xml
-        getLayoutInflater().inflate(R.layout.activity_search, contentFrameLayout);
-        Intent intent = new Intent();
+        setContentView(R.layout.activity_search);
         String utente=SaveSharedPreference.getUserName(getApplicationContext());
         String mail=SaveSharedPreference.getEmail(getApplicationContext());
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        View header = navigationView.getHeaderView(0);
-        TextView nome = (TextView) header.findViewById(R.id.nome_utente);
-        nome.setText(utente);
-        TextView email = (TextView) header.findViewById(R.id.email);
-        email.setText(mail);
+        mDateDisplay = (TextView) findViewById(R.id.dateDisplay);
+        mPickDate = (Button) findViewById(R.id.pickDate);
+        mPickDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(0);
+            }
+        });
 
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
 
-
-
-
-
+        updateDisplay();
     }
+
+    protected void updateDisplay() {
+        mDateDisplay.setText(
+                new StringBuilder()
+                        .append(mMonth + 1).append("-")
+                        .append(mDay).append("-")
+                        .append(mYear).append(" "));
+    }
+    protected DatePickerDialog.OnDateSetListener mDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker view, int year,
+                                      int monthOfYear, int dayOfMonth) {
+                    mYear = year;
+                    mMonth = monthOfYear;
+                    mDay = dayOfMonth;
+                    updateDisplay();
+                }
+            };
+
+    protected Dialog onCreateDialog(int id) {
+        return new DatePickerDialog(this,
+                mDateSetListener,
+                mYear, mMonth, mDay);
+    }
+
+
     }
 
