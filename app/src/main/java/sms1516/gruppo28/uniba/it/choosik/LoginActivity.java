a39d6663833e8b9b83c21bced9e9e4193521e517
ivan <ivan.ardillo@gmail.com>
2016-10-27 13:17:03 +0200

    cambiata modalita' del login con json

--- app/src/main/java/sms1516/gruppo28/uniba/it/choosik/LoginActivity.java
+++ app/src/main/java/sms1516/gruppo28/uniba/it/choosik/LoginActivity.java
@@ -2,15 +2,121 @@ package sms1516.gruppo28.uniba.it.choosik;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText usernameField,passwordField;
+    private EditText usernameField, passwordField;
+
+
+    private class JsonLoginTask extends AsyncTask<String,Void,String>{
+        JSONObject loginresult;
+        String username;
+        String email;
+        boolean artista;
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try {
+                // salva in loginresult gli oggetti in formato json ricevuti dalla url
+                loginresult = new JSONObject(s);
+                Log.d("login result", loginresult.getJSONArray("objects").get(0).toString());
+
+                // prende come array di oggetti json tutti i dizionari della risposta, se
+                // ce n'e' 1, vuol dire che la combinazione username psw e' corretta
+                if (loginresult.getJSONArray("objects").length() == 1){
+                    JSONObject datiutente = loginresult.getJSONArray("objects").getJSONObject(0);
+                    // preleva username e psw dell'utente loggato correttamente
+                    username = datiutente.getString("username");
+                    artista = datiutente.getBoolean("artista");
+                    email = datiutente.getString("email");
+                    // salva i dati nelle preferenze essendo corretti per evitare in seguito di
+                    // rifare la login
+                    SaveSharedPreference.setUserName(getApplicationContext(), username);
+                    SaveSharedPreference.setIsArtist(getApplicationContext(), artista);
+                    SaveSharedPreference.setEmail(getApplicationContext(), email);
+                    // avvia la mainActivity
+                    getApplicationContext()
+                            .startActivity(new Intent(getApplicationContext(), MainActivity.class)
+                                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                                    .putExtra("Username", username)
+                                    .putExtra("artista", artista)
+                                    .putExtra("Email", email));
+                } else {
+                    // credenziali errate
+                    Toast.makeText(getApplicationContext(),"Autenticazione fallita",Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e){
+                Log.e("errore", e.getMessage());
+            }
+            super.onPostExecute(s);
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,43 +138,28 @@ public class LoginActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        usernameField = (EditText)findViewById(R.id.txtUser);
-        passwordField = (EditText)findViewById(R.id.txtPassword);
+        usernameField = (EditText) findViewById(R.id.txtUser);
+        passwordField = (EditText) findViewById(R.id.txtPassword);
         final Button loginbtn = (Button) findViewById(R.id.loginbtn);
-//        loginbtn.setOnClickListener(new View.OnClickListener()
-//            {
-//                public void onClick(View v)
-//                {
-//                    login(v);
-//                    // Perform action on click
-//                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
-//                    startActivity(i);
-//                    //controllo credenziali login
-//                }
-//            }
-//             );
-
-
     }
 
     /**
+     * @param view Il metodo è invocato dal button di "activity_login"
      *
-     * @param view
-     * Il metodo è invocato dal button di "activity_login"
-     * Effettua un task del tipo SigninTask con i parametri username e password
      */
     public void login(View view) {
-        SigninTask st = new SigninTask(this);
         String username = usernameField.getText().toString();
         String password = passwordField.getText().toString();
-        st.execute(username, password);
+        new JsonLoginTask().execute("http://exrezzo.pythonanywhere.com/api/utente/?username=" +
+                username +
+                "&password=" +
+                password);
 
     }
 
-    public void register (View view){
-//        Intent i = new Intent(LoginActivity.this,RegisterActivity.class);
-//        startActivity(i);
+    public void register(View view) {
         Context context = this;
         context.startActivity(new Intent(context, RegisterActivity.class));
     }
+
 }
