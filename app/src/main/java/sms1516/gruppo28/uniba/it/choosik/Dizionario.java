dfe94dd96ae7699ff23e75329b645d08dc22191b
Giorgio Papa <giorgiopapa94@gmail.com>
2016-10-28 12:55:23 +0200

    Aggiunta hashma provincia sigla e classe dizionario

---
+++ app/src/main/java/sms1516/gruppo28/uniba/it/choosik/Dizionario.java
@@ -0,0 +1,51 @@
+package sms1516.gruppo28.uniba.it.choosik;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Created by Giorgio on 28/10/2016.
+ */
+
+public class Dizionario {
+    static HashMap dizProv;
+
+
+    public static HashMap dizionarioProvicia() {
+        String[] nomiProvince = {
+                "Agrigento", "Alessandria", "Ancona", "Aosta", "Aquila", "Arezzo", "Ascoli Piceno",
+                "Asti", "Avellino", "Bari", "Belluno", "Benevento", "Bergamo", "Biella", "Bologna",
+                "Bolzano", "Brescia", "Brindisi", "Cagliari", "Caltanissetta", "Campobasso", "Caserta",
+                "Catania", "Catanzaro", "Chieti", "Como", "Cosenza", "Cremona", "Crotone", "Cuneo",
+                "Enna", "Ferrara", "Firenze", "Foggia", "Forlì e Cesena", "Frosinone", "Genova",
+                "Gorizia", "Grosseto", "Imperia", "Isernia", "La Spezia", "Latina", "Lecce", "Lecco",
+                "Livorno", "Lodi", "Lucca", "Macerata", "Mantova", "Massa-Carrara", "Matera", "Messina",
+                "Milano", "Modena", "Napoli", "Novara", "Nuoro", "Oristano", "Padova", "Palermo", "Parma",
+                "Pavia", "Perugia", "Pesa ro e Urbino", "Pescara", "Piacenza", "Pisa", "Pistoia",
+                "Pordenone", "Potenza", "Prato", "Ragusa", "Ravenna", "Reggio Calabria", "Reggio Emilia",
+                "Rieti", "Rimini", "Roma", "Rovigo", "Salerno", "Sassari", "Savona", "Siena", "Siracusa",
+                "Sondrio", "Taranto", "Teramo", "Terni", "Torino", "Trapani", "Trento", "Treviso",
+                "Trieste", "Udine", "Varese", "Venezia", "Verbano-Cusio-Ossola", "Vercelli", "Verona",
+                "Vibo Valentia", "Vicenza", "Viterbo"};
+
+        String[] siglaprovincie = {
+                "AG", "AL", "AN", "AO", "AR", "AP", "AT", "AV", "BA", "BT", "BL", "BN", "BG",
+                "BI", "BO", "BZ", "BS", "BR", "CA", "CL", "CB", "CI", "CE", "CT", "CZ", "CH",
+                "CO", "CS", "CR", "CR", "CN", "EN", "FM", "FE", "FI", "FG",
+                "FC", "FR", "GE", "GO", "GR", "IM", "IS", "SP", "AQ", "LT", "LE", "LC", "LI",
+                "LO", "LU", "MC", "MN", "MS", "MT", "ME", "MI", "MO", "MB", "NA", "NO", "NU", "OT",
+                "OR", "PD", "PA", "PR", "PV", "PG", "PU", "PE", "PC", "PI", "PT", "PN", "PZ", "PO",
+                "RG", "RA", "RC", "RE", "RI", "RN", "RM", "RO", "SA",
+                "VS", "SS", "SV", "SI", "SR", "SO", "TA", "TE", "TR", "TO", "TP", "TN", "TV", "TS",
+                "UD", "VA", "VE", "VB", "VC", "VR", "VV", "VI", "VT",};
+
+
+        dizProv = new HashMap<String, String>();
+        for (int i = 0; i < nomiProvince.length; i++) {
+            dizProv.put(nomiProvince[i], siglaprovincie[i]);
+        }
+        return dizProv;
+
+    }
+}
--- app/src/main/java/sms1516/gruppo28/uniba/it/choosik/SearchActivity.java
+++ app/src/main/java/sms1516/gruppo28/uniba/it/choosik/SearchActivity.java
@@ -10,13 +10,13 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 
 import java.util.Calendar;
+import java.util.HashMap;
 
 /**
  * La classe Search Activity e' estesa a MainActivity in modo tale da implementare
  * il nav_menu
  */
-public class SearchActivity extends AppCompatActivity
-{
+public class SearchActivity extends AppCompatActivity {
     /**
      * Nel metodo onCreate carichiamo lo stesso menu della Main Activity
      * Si implementa un Date Picker per gestire le date in input
@@ -27,37 +27,6 @@ public class SearchActivity extends AppCompatActivity
     int mYear;
     int mMonth;
     int mDay;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
-        String utente=SaveSharedPreference.getUserName(getApplicationContext());
-        String mail=SaveSharedPreference.getEmail(getApplicationContext());
-        mDateDisplay = (TextView) findViewById(R.id.dateDisplay);
-        mPickDate = (Button) findViewById(R.id.pickDate);
-        mPickDate.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                showDialog(0);
-            }
-        });
-
-        final Calendar c = Calendar.getInstance();
-        mYear = c.get(Calendar.YEAR);
-        mMonth = c.get(Calendar.MONTH);
-        mDay = c.get(Calendar.DAY_OF_MONTH);
-
-        updateDisplay();
-    }
-
-    protected void updateDisplay() {
-        mDateDisplay.setText(
-                new StringBuilder()
-                        .append(mMonth + 1).append("-")
-                        .append(mDay).append("-")
-                        .append(mYear).append(" "));
-    }
     protected DatePickerDialog.OnDateSetListener mDateSetListener =
             new DatePickerDialog.OnDateSetListener() {
                 public void onDateSet(DatePicker view, int year,
@@ -69,6 +38,38 @@ public class SearchActivity extends AppCompatActivity
                 }
             };
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_search);
+        String utente = SaveSharedPreference.getUserName(getApplicationContext());
+        String mail = SaveSharedPreference.getEmail(getApplicationContext());
+        mDateDisplay = (TextView) findViewById(R.id.dateDisplay);
+        HashMap diz = Dizionario.dizionarioProvicia();
+        mPickDate = (Button) findViewById(R.id.pickDate);
+        mPickDate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showDialog(0);
+            }
+        });
+
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+        updateDisplay();
+    }
+
+    protected void updateDisplay() {
+        mDateDisplay.setText(
+                new StringBuilder()
+                        .append(mMonth + 1).append("-")
+                        .append(mDay).append("-")
+                        .append(mYear).append(" "));
+    }
+
     protected Dialog onCreateDialog(int id) {
         return new DatePickerDialog(this,
                 mDateSetListener,
@@ -76,5 +77,5 @@ public class SearchActivity extends AppCompatActivity
     }
 
 
-    }
+}
 
