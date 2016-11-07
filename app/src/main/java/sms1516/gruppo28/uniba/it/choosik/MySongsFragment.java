35787a088cbd6167e8236ebd827d1cb60f61fd3d
ivan <ivan.ardillo@gmail.com>
2016-11-07 11:43:27 +0100

    rimozione canzoni artista

--- app/src/main/java/sms1516/gruppo28/uniba/it/choosik/MySongsFragment.java
+++ app/src/main/java/sms1516/gruppo28/uniba/it/choosik/MySongsFragment.java
@@ -2,6 +2,7 @@ package sms1516.gruppo28.uniba.it.choosik;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -9,15 +10,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -32,18 +39,20 @@ public class MySongsFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Le mie canzoni");
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_mysongs,container,false);
         final ListView listView = (ListView) rootView.findViewById(R.id.list_songs);
-        populateSongsList(listView);
+        populateSongsList(listView, inflater);
 
 
         return rootView;
     }
 
-    private void populateSongsList(final ListView listView){
+    ArrayList<String> titoliCanzoniList;
+    ArrayList<Integer> idCanzoniList;
+
+    private void populateSongsList(final ListView listView, final LayoutInflater inflater){
         AsyncHttpClient client = new AsyncHttpClient();
         client.get("http://exrezzo.pythonanywhere.com/api/canzone/?autore__username=" +
                 SaveSharedPreference.getUserName(getContext()), new JsonHttpResponseHandler() {
@@ -53,12 +62,54 @@ public class MySongsFragment extends Fragment {
                 progress.dismiss();
                 try {
                     JSONArray canzoniArrayJson = response.getJSONArray("objects");
-                    String[] titoliCanzoniArray = new String[canzoniArrayJson.length()];
+                    titoliCanzoniList = new ArrayList();
+                    idCanzoniList = new ArrayList();
                     for (int i = 0; i < canzoniArrayJson.length(); i++) {
-                        titoliCanzoniArray[i] = canzoniArrayJson.getJSONObject(i).getString("titolo");
+                        titoliCanzoniList.add(canzoniArrayJson.getJSONObject(i).getString("titolo"));
+                        idCanzoniList.add(canzoniArrayJson.getJSONObject(i).getInt("id"));
                     }
+                    //ArrayList<String> titoliCanzoniList = (ArrayList<String>) Arrays.asList(titoliCanzoniArray);
+                    // idCanzoniList = (ArrayList<Integer>) Arrays.asList(idCanzoniArray);
+                    final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, titoliCanzoniList){
 
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, titoliCanzoniArray);
+                        @NonNull
+                        @Override
+                        public View getView(final int position,View convertView, ViewGroup parent) {
+                            if (convertView == null) {
+                                convertView = inflater.inflate(R.layout.list_item_delete, parent, false);
+
+                            }
+
+                            ImageButton imgBtn = (ImageButton) convertView.findViewById(R.id.delete_img);
+                            TextView textItem = (TextView) convertView.findViewById(R.id.text_item);
+                            textItem.setText(titoliCanzoniList.get(position));
+
+                            imgBtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    AsyncHttpClient client = new AsyncHttpClient();
+                                    Log.e("URL 2delete", "http://exrezzo.pythonanywhere.com/api/canzone/" + idCanzoniList.get(position)+"/");
+                                    client.delete(getContext(), "http://exrezzo.pythonanywhere.com/api/canzone/" + idCanzoniList.get(position)+"/",
+                                            new AsyncHttpResponseHandler() {
+                                                @Override
+                                                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                                    Log.e("Delete canzone ok id:",titoliCanzoniList.get(position));
+                                                    remove(getItem(position));
+                                                    notifyDataSetChanged();
+                                                    idCanzoniList.remove(position);
+                                                }
+
+                                                @Override
+                                                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                                                    Log.e("Delete canzone NO id",titoliCanzoniList.get(position));
+                                                }
+                                            });
+                                }
+                            });
+
+                            return convertView;
+                        }
+                    };
                     listView.setAdapter(adapter);
 //                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //                        @Override
@@ -79,6 +130,9 @@ public class MySongsFragment extends Fragment {
                 } catch (JSONException e) {
                     e.printStackTrace();
                     Log.e("errore json", e.getMessage());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.e("Errore", e.getMessage());
                 }
             }
 
